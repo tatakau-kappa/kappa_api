adcab26d73d5f250697303015562c28b1c0a855b
Tadayuki Onishi <tt.tanishi100@gmail.com>
2016-09-18 03:59:38 +0900

    enable showing video without login

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  skip_before_action :authenticate_user_from_token!, only: [:update]
+  skip_before_action :authenticate_user_from_token!, only: [:show, :update]
 
   def index
     render json: Video.includes(:video_comments).order(created_at: :desc)
