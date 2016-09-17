20fb68f347e24f2bc0acbcbaa4d3da4ce5dc06d3
Tadayuki Onishi <tt.tanishi100@gmail.com>
2016-09-18 07:23:07 +0900

    fix

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     user
     video_uid { "#{Devise.friendly_token[0,20]}.mp4" }
     image_uid { "#{Devise.friendly_token[0,20]}.jpg" }
-    swapped_uid { "#{Devise.friendly_token[0,20]}.mp4" }
-    thumbnail_uid { "#{Devise.friendly_token[0,20]}.jpg" }
+    swapped_uid { "/processed/#{Devise.friendly_token[0,20]}.mp4" }
+    thumbnail_uid { "/processed/#{Devise.friendly_token[0,20]}.jpg" }
     program_name '番組名'
   end
 end
