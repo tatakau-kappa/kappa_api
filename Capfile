7a21b87bfdf6a18edf50824f3672fb402f11084f
Tadayuki Onishi <tt.tanishi100@gmail.com>
2016-09-18 11:51:11 +0900

    fix deploy script

--- Capfile
+++ Capfile
@@ -8,7 +8,6 @@ require 'capistrano/puma'
 require 'capistrano/rails/migrations'
 require 'seed-fu/capistrano3'
 require 'capistrano/sidekiq'
-require 'capistrano/sidekiq/monit'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -33,8 +33,6 @@ set :puma_pid, -> { "#{shared_path}/tmp/puma/pid" }
 set :original_preload_app, fetch(:puma_preload_app)
 set :puma_preload_app, -> { fetch(:original_preload_app) || !!ENV['FORCE_RESTART'] }
 
-set :sidekiq_monit_use_sudo, false
-
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:app), in: :sequence do
